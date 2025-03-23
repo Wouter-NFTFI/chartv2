789918c69c5b9ff81519d64fe48d4b5f948c4236
Wouter Schreuders <wschreuders@gmail.com>
2025-03-23 12:17:03 +0400

    Create organized folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+// Follow rules from .cursor-ruleset.md
+// Get latest NFT floor price from verified contract ABI
+// Do not use fallbacks, mock data, or normalized fields
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/global/index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
